305c5bd1f4d1c7bdf3c646d232250fa511272ee0
Bruno Lin <50309943+Mananoy@users.noreply.github.com>
2022-03-13 16:17:48 +1030

    Delete app directory

--- app/models/enrolment.rb
+++
@@ -1,2 +0,0 @@
-class Enrolment < ApplicationRecord
-end
--- app/models/student.rb
+++
@@ -1,8 +0,0 @@
-class Student < ApplicationRecord
-  has_secure_password
-
-  def self.get_name(s_id)
-    name = Student.where(account: s_id).pluck(:name).first
-  end
-
-end
